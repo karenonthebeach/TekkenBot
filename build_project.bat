f2d37b4c55e6c2df05ae3dbbb7600fc0885e7d59
WAZAAAAA <WAZAAAAA0@users.noreply.github.com>
2018-06-02 03:47:51 +0200

    building instructions

--- build_project.bat
+++ build_project.bat
@@ -1,1 +1,6 @@
+::install the pyinstaller requirement first if missing:
+::pip install pyinstaller
 pyinstaller --windowed --clean --icon=TekkenData/tekken_bot_close.ico --add-data TekkenData;TekkenData  --name TekkenBotPrime GUI_TekkenBotPrime.py
+::alternative sample command for building:
+::"C:%HOMEPATH%\AppData\Local\Programs\Python\Python36-32\Scripts\pyinstaller.exe" --windowed --clean --icon=TekkenData/tekken_bot_close.ico --add-data TekkenData;TekkenData  --name TekkenBotPrime GUI_TekkenBotPrime.py
+pause
